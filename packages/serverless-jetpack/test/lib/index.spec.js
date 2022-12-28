39b3c2430caf5c62cf9e0ad43488944e5b5400d0
Ryan Roemer <ryan.roemer@formidable.com>
2022-12-28 15:30:23 -0800

    Working test harness with mock fs, but slow...

--- packages/serverless-jetpack/test/lib/index.spec.js
+++ packages/serverless-jetpack/test/lib/index.spec.js
@@ -1,9 +1,11 @@
 "use strict";
 
+const path = require("path");
+
 const mock = require("mock-fs");
 const { createSandbox } = require("sinon");
-
 const { createServerless } = require("../util/serverless");
+const { zipContents } = require("trace-pkg/test/util/file");
 
 describe("lib/index", () => {
   let sandbox;
@@ -23,9 +25,49 @@ describe("lib/index", () => {
   });
 
   describe("TODO", () => {
-    it("TODO lib tests", async () => {
-      serverless = await createServerless({ sandbox });
-      console.log("TODO HERE", { serverless }); // eslint-disable-line no-console
+    it("can do basic Serverless packaging", async function () {
+      // TODO REMOVE
+      this.timeout(5000); // eslint-disable-line no-magic-numbers,no-invalid-this
+
+      // TODO: Abstract all of this / find a different solution to the lazy require() problem in SLS
+      // Go to project root to load mocks.
+      process.chdir(path.resolve(__dirname, "../../../../"));
+      mock({
+        packages: {
+          "serverless-jetpack": {
+            // We will end up here for CWD.
+            "serverless.yml": `
+              service: sls-mocked
+
+              provider:
+                name: aws
+                runtime: nodejs16.x
+
+              package:
+                patterns:
+                  - "!**"
+                  - "serverless.yml"
+
+              functions:
+                one:
+                  handler: one.handler
+            `,
+            node_modules: mock.load(path.resolve(__dirname, "../../node_modules"))
+          }
+        },
+        node_modules: mock.load(path.resolve(__dirname, "../../../../node_modules"))
+      });
+
+      // Go back to package root for tests.
+      process.chdir(path.resolve(__dirname, "../.."));
+
+      serverless = await createServerless();
+
+      await serverless.run();
+
+      expect(zipContents(".serverless/sls-mocked.zip")).to.eql([
+        "serverless.yml"
+      ]);
     });
   });
 });
--- packages/serverless-jetpack/test/util/serverless.js
+++ packages/serverless-jetpack/test/util/serverless.js
@@ -1,23 +1,33 @@
 "use strict";
 
 const Serverless = require("serverless");
+const readConfiguration = require("serverless/lib/configuration/read");
+const resolveConfigurationPath = require("serverless/lib/cli/resolve-configuration-path");
+const { stub } = require("sinon");
 
 // [BRITTLE]: Various lazy requires in `init()` which conflict with mock-fs, so we import other
 // files that force the early imports.
-// - lazy `require("@serverless/dashboard-plugin'")`
 require("serverless/lib/cli/handle-error");
-// - lazy `require("serverless/lib/plugins")`
 require("serverless/lib/plugins");
 
 // Wrap serverless to allow "normal" usage with plugin injected.
-const createServerless = async ({ sandbox }) => {
+const createServerless = async ({ options = {}, commands = ["package"] } = {}) => {
+  // Mimic `serverless.js` CLI script setup.
+  const serviceDir = process.cwd();
+  const configurationPath = await resolveConfigurationPath();
+  const configurationFilename = configurationPath.slice(serviceDir.length + 1);
+  const configuration = await readConfiguration(configurationPath);
+
   const serverless = new Serverless({
-    options: {},
-    commands: []
+    serviceDir,
+    options,
+    commands,
+    configuration,
+    configurationFilename
   });
   await serverless.init();
   serverless.cli = {
-    log: sandbox.stub()
+    log: stub()
   };
 
   // TODO: Refactor to maybe be { serverless, restore }
